5296c8245470b2f0e1ee30d86218db09594a5ef7
Isaac E. Louzeiro <isaacelias1110@gmail.com>
2022-01-25 11:05:17 -0300

    adicionando conteudo inicial

---
+++ src/components/EmManutencao.vue
@@ -0,0 +1,58 @@
+<template>
+    <section>
+        <h1>A criar...</h1>
+        <h2>Codando um portifólio diferente do que você viu por ai!</h2>
+        <h3>Novidades em breve</h3>
+        <a href="https://github.com/IsaacLouzeiro/">Meu Github</a>        
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'EmManutencao',
+    data() {
+        return {
+            
+        }
+    },
+    props: {
+        situacao: Boolean
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    h1, h2, h3 {
+        margin: 5px 0;
+    }
+    
+    h1 {
+        font-size: 2em;
+        color: #2d3436;
+    }
+
+    h2 {
+        color: #636e72;
+    }
+
+    h3 {
+        color: #34495e;
+        margin-top: 1em;
+        margin-bottom: 0;
+    }
+
+    a {
+        display: inline-block;
+        padding: 10px 16px;
+        font-size: 1em;
+        text-decoration: none;
+        color: #fff;
+        background-color: #16a085;
+        border-radius: 5px;
+        margin-top: 10px;
+
+        &:hover {
+            background-color: #1abc9c;
+        }
+    }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <EmManutencao />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EmManutencao from '@/components/EmManutencao.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    EmManutencao
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+    .home {
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
