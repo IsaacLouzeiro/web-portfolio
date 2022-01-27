27e27bdc896b8d89b94a98169b95fc0ac0c40120
Isaac E. Louzeiro <isaacelias1110@gmail.com>
2022-01-27 20:07:27 -0300

    s

---
+++ src/components/NavBar.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav>
+        <div class="box-lang dropdown">
+            <button class="dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown button
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+            </ul>
+        </div>
+
+        <ul class="nav flex-column list-group" id="list-example">
+            <li class="nav-item">
+                <a class="nav-link" href="#Home">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#About">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#Work">Work</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#Contact">Contact</a>
+            </li>
+        </ul>
+
+        <div class="social-network">
+            <a href="https://github.com/IsaacLouzeiro"></a>
+            <a href="https://www.linkedin.com/in/isaac-louzeiro/"></a>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavBar'
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '../assets/scss/colors.scss';
+
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,27 @@
 <template>
   <div class="home">
-    <div v-show="this.manut == 1">
+    <div v-show="this.manut == 1" class="manutencao">
         <EmManutencao />
     </div>
 
-    <!-- seção principal -->
+    <!-- site funcional -->
     <section v-show="this.manut == 0" class="principal">
-        oi
+        <!-- menu navegação -->
+        <NavBar />
+        <!-- conteudo principal -->
+        <main>
+            
+        </main>
+        <!-- // conteudo principal -->
     </section>
+    <!-- // site funcional -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import EmManutencao from '@/components/EmManutencao.vue'
+import NavBar from '@/components/NavBar.vue'
 
 export default {
   name: 'Home',
@@ -24,14 +32,28 @@ export default {
       }
   },
   components: {
-    EmManutencao
+    EmManutencao,
+    NavBar
   }
 }
 </script>
 
 <style lang="scss" scoped>
-    .home {
+    @import '../assets/scss/colors.scss';
+
+    .home, .principal {
         height: 100%;
-        
+        background-color: $color3;
     }
+
+    .manutencao {
+        border: 1px solid;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: $color5;
+    }
+
+
 </style>
