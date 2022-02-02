1b61e733112fba32dd5253c2e2fc7a86565e74e0
Isaac E. Louzeiro <isaacelias1110@gmail.com>
2022-02-02 20:42:23 -0300

    adicionando preloader

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,33 @@
 <template>
+    <CarregadorSite v-show="montado === 0" />
     <router-view/>
 </template>
+
+<script>
+    import CarregadorSite from './components/CarregadorSite.vue';
+
+    export default {
+
+        data() {
+            return {
+                montado: null
+            }
+        },
+        components: {
+            CarregadorSite
+        },
+        beforeMount() {
+            this.montado = 0
+
+            
+        },
+        mounted() {
+            setTimeout(() => {
+                this.montado = 1
+            }, 2500);
+        }
+    }
+</script>
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap');
@@ -22,5 +49,11 @@ html, body, #app {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+#CarregadorSite {
+    position: absolute;
+    z-index: 10000000000;
+    overflow: hidden;
 }
 </style>
---
+++ src/components/CarregadorSite.vue
@@ -0,0 +1,42 @@
+<template>
+    <section id="CarregadorSite">
+        <img src="../assets/img/logo-isaac-louzeiro-tiny.png" alt="my logo">
+    </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '../assets/scss/colors.scss';
+
+    #CarregadorSite {
+        background-color: $color3;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        @keyframes loading {
+            0% {
+                margin-bottom: 0;
+            }
+            50% {
+                margin-bottom: 15px;
+            }
+            100% {
+                margin-bottom: 0;
+            }
+        }
+
+        img {
+            position: relative;
+            width: 150px;
+            animation: loading 1.5s linear infinite;
+        }
+    }
+</style>
